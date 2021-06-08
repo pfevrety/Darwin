288861717d0e8f4391940f3b35f9af0130b896d9
Fevret <60936445+pfevrety@users.noreply.github.com>
2021-06-08 15:03:00 +0200

    🚸Support UX

--- commands/utility/support.js
+++ commands/utility/support.js
@@ -5,23 +5,13 @@ module.exports = class extends Command {
     constructor(...args) {
       super(...args, {
         name: 'support',
-        description: `Sends you Pogy's Support server`,
+        description: `Sends you Darwin's Support server`,
         category: 'Utility',
         cooldown: 3
       });
     }
 
     async run(message) {
-     const guildDB = await Guild.findOne({
-        guildId: message.guild.id
-      });
-    
-      const language = require(`../../data/language/${guildDB.language}.json`)
-      
-      const embed = new Discord.MessageEmbed()
-        .setColor(message.guild.me.displayHexColor)
-        .setDescription(`${language.support}(https://discord.gg/FqdH4sfKBg) ${message.client.emoji.success}`);
-
-      await message.channel.send(embed)  
+      await message.reply("https://discord.gg/UssYQRgsvn")
     }
 };
