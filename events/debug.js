7a400ed7c70e51c457673ac5a7548f34f8331259
Fevret <60936445+pfevrety@users.noreply.github.com>
2021-06-08 15:02:15 +0200

    ♻️Refactor code

--- events/debug.js
+++ events/debug.js
@@ -1,4 +1,3 @@
-const muteModel = require('../models/mute');
 const Event = require('../structures/Event');
 const Discord = require('discord.js');
 const config = require('../config.json');
@@ -8,38 +7,37 @@ const Maintenance = require('../database/schemas/maintenance')
 let number = 1
 module.exports = class extends Event {
     async run(info) {
- 
-let embed;
-  const maintenance = await Maintenance.findOne({maintenance: 'maintenance'})
 
-  
-  if(info.includes('hit')){
+        let embed;
+        const maintenance = await Maintenance.findOne({maintenance: 'maintenance'})
 
 
-number = ++number
-
-embed = `${info} - ${number}`;
-logger.info(info, { label: 'Debug' })
+        if (info.includes('hit')) {
 
 
-if(number >= 10){
-  embed = `${info} - ${number} - SAFE MODE REACHED <@710465231779790849>`;
-  console.log('Safe mode reached - Turning maintenance mode on.')
-  maintenance.toggle = "true"
-  await maintenance.save();
-  process.exit(1);
- 
-}
+            number = ++number
 
-const lmao = new Discord.MessageEmbed()
-.setDescription(embed)
-.setColor('RED')
+            embed = `${info} - ${number}`;
+            logger.info(info, {label: 'Debug'})
 
 
-webhookClient.send(lmao)
-  }
-  
-  
+            if (number >= 10) {
+                embed = `${info} - ${number} - SAFE MODE REACHED <@710465231779790849>`;
+                console.log('Safe mode reached - Turning maintenance mode on.')
+                maintenance.toggle = "true"
+                await maintenance.save();
+                process.exit(1);
+
+            }
+
+            const lmao = new Discord.MessageEmbed()
+                .setDescription(embed)
+                .setColor('RED')
+
+
+            webhookClient.send(lmao)
+        }
+
 
     }
 }
