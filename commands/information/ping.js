8e4205927b6ae3f49bd4c7636f7f232e09f01e9e
Fevret <60936445+pfevrety@users.noreply.github.com>
2021-06-08 15:03:45 +0200

    ⚡️Improve ping command

--- commands/information/ping.js
+++ commands/information/ping.js
@@ -1,58 +1,61 @@
 const Command = require('../../structures/Command');
-const { MessageEmbed } = require('discord.js');
-const { oneLine, stripIndent } = require('common-tags');
-const ms = require("parse-ms");
+const {MessageEmbed} = require('discord.js');
 const Guild = require('../../database/schemas/Guild');
 module.exports = class extends Command {
     constructor(...args) {
-      super(...args, {
-        name: 'ping',
-        aliases: ['latency'],
-        description: 'Check\'s Pogy\'s latency',
-        category: 'Information',
-        cooldown: 5
-      });
+        super(...args, {
+            name: 'ping',
+            aliases: ['latency'],
+            description: 'Check\'s Darwin\'s latency',
+            category: 'Information',
+            cooldown: 5
+        });
     }
 
     async run(message) {
 
-            const client = message.client
-      const guildDB = await Guild.findOne({
-        guildId: message.guild.id
-      });
-    
-      const language = require(`../../data/language/${guildDB.language}.json`)
+        const client = message.client
+        const guildDB = await Guild.findOne({
+            guildId: message.guild.id
+        });
 
-      const embed = new MessageEmbed()
-      .setDescription(`\`${language.pinging}\``)
-      .setColor(message.guild.me.displayHexColor)  
-      .setFooter(`Shard #${message.guild.shardID}`);
+        const language = require(`../../data/language/${guildDB.language}.json`)
 
-    const msg = await message.channel.send(embed);
+        const embed = new MessageEmbed()
+            .setTitle(language.ping_calcul)
+            .setDescription(`\`${language.pinging}\``)
+            .setColor(message.guild.me.displayHexColor)
+            .setTimestamp()
+            .addField(
+                '\u200b',
+                '**[Invite](https://invite.darwin.xyz) | ' +
+                '[Support Server](https://darwin.xyz/support) | ' +
+                '[Dashboard](https://darwin.xyz/dashboard)**')
+            .setFooter(`Darwin - pfevrety#1908`, message.client.user.displayAvatarURL({dynamic: true}))
 
-const latency = msg.createdTimestamp - message.createdTimestamp;
+        const msg = await message.channel.send(embed);
 
-let koko = stripIndent`
-**${language.timeTaken}** \`${latency}ms\`
-**${language.discordAPI}** \`${Math.round(this.client.ws.ping)}ms\`
-`
+        const latency = msg.createdTimestamp - message.createdTimestamp;
+
+        let koko = (language.ping_info).replace("{ping1}", latency)
+            .replace("{ping2}",
+                Math.round(this.client.ws.ping)
+            )
+
+        let color = message.guild.me.displayHexColor
+        if (latency < 100) {
+            color = `#00ff00`
+        } else if (latency > 100 && latency < 300) {
+            color = `#CCCC00`
+        } else if (latency >= 300) {
+            color = message.client.color.red
+        } else color = message.guild.me.displayHexColor
 
 
-let color = message.guild.me.displayHexColor
-if (latency < 100) {
-  color = `#00ff00`
-} else if (latency > 100 && latency < 200){
-  color = `#CCCC00`
-} else if (latency > 200){
-  color = message.client.color.red
-} else color = message.guild.me.displayHexColor
+        embed.setDescription(`${koko}`)
+        embed.setColor(color)
+        msg.edit(embed);
 
-
-      embed.setDescription(`${koko}`)
-      embed.setColor(color)
-    msg.edit(embed);
-
-      
 
     }
 };
