9ad506ea93ca7d81ff4dd74670ff0f7cf6c01280
Fevret <60936445+pfevrety@users.noreply.github.com>
2021-06-08 14:51:14 +0200

    🎨Improve change prefix

--- commands/config/prefix.js
+++ commands/config/prefix.js
@@ -4,40 +4,41 @@ const discord = require("discord.js")
 
 module.exports = class extends Command {
     constructor(...args) {
-      super(...args, {
-        name: 'setprefix',
-        description: 'Sets the prefix for this server',
-        category: 'Config',
-        usage: [ '<prefix>' ],
-        examples: [ 'setprefix !'],
-        cooldown: 3,
-        guildOnly: true,
-        userPermission: ['MANAGE_GUILD'],
-      });
+        super(...args, {
+            name: 'setprefix',
+            description: 'Sets the prefix for this server',
+            aliases: ['set-prefix', 'prefix'],
+            category: 'Config',
+            usage: ['<prefix>'],
+            examples: ['setprefix ``!``'],
+            cooldown: 3,
+            guildOnly: true,
+            userPermission: ['MANAGE_GUILD'],
+        });
     }
 
     async run(message, args) {
-      const settings = await Guild.findOne({
-        guildId: message.guild.id,
-      }, (err, guild) => {
-        if (err) console.log(err)
-      });
+        const settings = await Guild.findOne({
+            guildId: message.guild.id,
+        }, (err, guild) => {
+            if (err) console.log(err)
+        });
 
-      const guildDB = await Guild.findOne({
-        guildId: message.guild.id
-      });
-      
-
-      const language = require(`../../data/language/${guildDB.language}.json`);
+        const guildDB = await Guild.findOne({
+            guildId: message.guild.id
+        });
 
 
-      if(!args[0]) return message.channel.send(new discord.MessageEmbed().setColor(message.client.color.red).setDescription(`${message.client.emoji.fail} ${language.setPrefixMissingArgument}`))
-  
-      if(args[0].length > 5) return message.channel.send(new discord.MessageEmbed().setColor(message.client.color.red).setDescription(`${message.client.emoji.fail} ${language.setPrefixLongLength}`))
-      
-      message.channel.send(new discord.MessageEmbed().setColor(message.client.color.green).setDescription(`${message.client.emoji.success} ${language.setPrefixChange.replace("{prefix}", args[0])}`))
-           await settings.updateOne({
-                prefix: args[0]
-            });
+        const language = require(`../../data/language/${guildDB.language}.json`);
+
+
+        if (!args[0]) return message.channel.send(new discord.MessageEmbed().setColor(message.client.color.red).setDescription(`${message.client.emoji.fail} ${language.setPrefixMissingArgument}`))
+
+        if (args[0].length > 5) return message.channel.send(new discord.MessageEmbed().setColor(message.client.color.red).setDescription(`${message.client.emoji.fail} ${language.setPrefixLongLength}`))
+
+        message.channel.send(new discord.MessageEmbed().setColor(message.client.color.green).setDescription(`${message.client.emoji.success} ${language.setPrefixChange.replace("{prefix}", args[0])}`))
+        await settings.updateOne({
+            prefix: args[0]
+        });
     }
-  }    
+}
