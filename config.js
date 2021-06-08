aea6dfb36b8543a32abbcf270f4507fe78bb520b
Fevret <60936445+pfevrety@users.noreply.github.com>
2021-06-08 14:59:07 +0200

    🔧Edit config

--- config.js
+++ config.js
@@ -1,14 +1,13 @@
 module.exports = {
  "verification": "",
- "description": "Pogy is a multi-purpose discord bot ready to skill up and boost up your Discord server!",
- "domain": "", // domain
+ "description": "Darwin is a multi-purpose discord bot ready to skill up and boost up your Discord server!",
+ "domain": "localhost:5003", // domain
  "google_analitics": "", // google analitics
  "token": process.env.TOKEN,
  "https":"https://", // leave as is
  "port":"5003",
-
- "client_id":"", // bot client ID
- "secret":"" // bot client secret for auth
+ "client_id": process.env.CLIENTID, // bot client ID
+ "secret": process.env.SECRET // bot client secret for auth
 
 }
 
